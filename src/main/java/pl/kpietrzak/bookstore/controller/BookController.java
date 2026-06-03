3a9148f707b86dba77186b61f7c4e335e5c734a7
Krzysztof Pietrzak <krzysztofpietrzakx98@gmail.com>
2026-06-03 06:02:57 +0200

    feat: add book REST controller

---
+++ src/main/java/pl/kpietrzak/bookstore/controller/BookController.java
@@ -0,0 +1,58 @@
+package pl.kpietrzak.bookstore.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pl.kpietrzak.bookstore.dto.book.BookRequest;
+import pl.kpietrzak.bookstore.dto.book.BookResponse;
+import pl.kpietrzak.bookstore.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<BookResponse> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public BookResponse getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @GetMapping("/search/title")
+    public List<BookResponse> getBooksByTitle(@RequestParam String title) {
+        return bookService.searchBooksByTitle(title);
+    }
+
+    @GetMapping("/search/author")
+    public List<BookResponse> getBooksByAuthor(@RequestParam String author) {
+        return bookService.searchBooksByAuthor(author);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookResponse createBook(@Valid @RequestBody BookRequest bookRequest) {
+        return bookService.createBook(bookRequest);
+    }
+
+    @PutMapping("/{id}")
+    public BookResponse updateBook(@PathVariable Long id, @Valid @RequestBody BookRequest bookRequest) {
+
+        return bookService.updateBookBy(id, bookRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBookById(@PathVariable Long id) {
+        bookService.deleteBookBy(id);
+    }
+}
