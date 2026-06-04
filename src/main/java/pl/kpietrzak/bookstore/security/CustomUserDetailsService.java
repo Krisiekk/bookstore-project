f55504dc886f2c5eddff141a6e80dcb95707bae4
Krzysztof Pietrzak <krzysztofpietrzakx98@gmail.com>
2026-06-04 10:56:10 +0200

    feat: add custom user details service

---
+++ src/main/java/pl/kpietrzak/bookstore/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package pl.kpietrzak.bookstore.security;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.kpietrzak.bookstore.entity.User;
+import pl.kpietrzak.bookstore.repository.UserRepository;
+
+@Service
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+
+
+}
